3cc87135aefb01a350b8a62d354e0e8e06bb3688
raul <raul.partida.dev@gmail.com>
2024-04-01 16:27:25 -0600

    git initial

---
+++ app/src/main/java/com/example/horoscopo/ui/lucky/LuckyFragment.kt
@@ -0,0 +1,24 @@
+package com.example.horoscopo.ui.lucky
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.horoscopo.databinding.FragmentLuckyBinding
+
+
+class LuckyFragment : Fragment() {
+
+    private var _binding: FragmentLuckyBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLuckyBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
+}
